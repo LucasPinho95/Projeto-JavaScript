f56c576f41c526dc5a5acc51d577af655f8c2958
Lucas Pinho <lucas.pinho@outlook.com.br>
2020-06-29 17:05:12 -0300

    Progresso aula 1

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+var d = new Date();
+alert(d.getDay()+1);
+alert(d.getHours()+1);
+alert(d.getMinutes()+1);
+alert(d.getMonth()+1);
+
+/*
+var d = new Date();
+alert(d);
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+     alert(count);
+};
+*/
+
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade")
+};
+*/
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã","pêra","laranja"];
+//lista.push("uva");
+//lsita.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" / "));
+
+//var nome = "Lucas Pinho";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "Mansel é o melhor piloto do mundo"
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade*idade2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Mansel","Senna"));
